d075d34e9f84e38d8ba2fc5d7c759378c4cc6ecd
Mario Salles <mariofsalles@gmail.com>
2019-04-06 22:20:10 +0100

    Insert sell equation

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,6 +1,7 @@
 class Sell < ApplicationRecord
-  
+
   include Fae::BaseModelConcern
+  before_save :set_total
 
   enum status: { finished:0, cancelled: 1 }
   validates :client, presence: true
@@ -22,4 +23,17 @@ class Sell < ApplicationRecord
     order(:id)
   end
   
+  private
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
+
+    if self.discount.present?
+      total -= self.discount.value
+    end
+
+    total = (total >= 0)? total : 0
+    self.total = total
+  end 
 end
