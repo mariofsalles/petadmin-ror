4a17e40d1b6fef13f2693e07a317181f22d7b35a
Mario Salles <mariofsalles@gmail.com>
2019-04-06 10:26:33 +0100

    Adjusting tables of views

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,7 +1,10 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
+  belongs_to :discount
+  belongs_to :client
+  validates :client, presence: true
   enum status: { finished:0, cancelled: 1 }
-
+ 
   def fae_display_field
     id
   end
@@ -10,6 +13,4 @@ class Sell < ApplicationRecord
     order(:id)
   end
   
-  belongs_to :discount
-  belongs_to :client
 end
