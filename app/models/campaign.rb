d9e50e6a89caa3188342fee19429dfe5c00c728d
Mario Salles <mariofsalles@gmail.com>
2019-04-07 17:56:11 +0100

    Setting mailer

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -8,8 +8,17 @@ class Campaign < ApplicationRecord
   has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
 
+  after_save :schedule_emails
+
   def fae_display_field
     title
+  end
+  
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaignJob.perform_later client, self.title, self.body
+    end
   end
 
 end
